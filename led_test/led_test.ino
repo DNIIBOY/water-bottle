410f1ec8748781f709e5d4a5e756909a1e971e6d
DNIIBOY <daniel@nettelfield.com>
2022-04-27 12:49:57 +0200

    led test made

---
+++ led_test/led_test.ino
@@ -0,0 +1,43 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 144
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 10; // delay for half a second
+
+void setup() 
+{
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() 
+{
+  for(int i=0;i<NUMPIXELS;i++)
+  {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(64,0,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+
+  for(int i=0;i<NUMPIXELS;i++)
+  {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,64,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+
+  for(int i=0;i<NUMPIXELS;i++)
+  {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,0,64)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+}
